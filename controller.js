02bb1d10cd7b55744e9657c295f06ec7e773b940
zain <zain.faheem.2019@uni.strath.ac.uk>
2022-03-10 16:12:01 +0000

    added img folder and structure to classes

---
+++ controller.js
@@ -0,0 +1,6 @@
+'use strict';
+/*global Model, View */
+//Joins Model and View
+// - the "plumbing"
+const view = new View();
+const model = new Model();
---
+++ view.js
@@ -0,0 +1,8 @@
+'use strict';
+//Handles the HTML and CSS - all refs to HTML and CSS here
+
+class View {
+
+    constructor() {
+    }
+}
