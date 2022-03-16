97e3af7a1a9ecc6d727338aaf3bc776049f6df6b
zain <zain.faheem.2019@uni.strath.ac.uk>
2022-03-16 15:17:42 +0000

    fix: step tracker not updating value

--- model.js
+++ model.js
@@ -1,62 +1,70 @@
- 'use strict';
-function Model() {
-
-    this.wnt = {};
-    this.wnt.mobile = false;
-    this.wnt.ie = false;
-    this.wnt.steps = localStorage.steps;
-
-         //check if mobile browser is supported
-         if (navigator.userAgent.match(/Android/i)
-             || navigator.userAgent.match(/webOS/i)
-             || navigator.userAgent.match(/iPhone/i)
-             || navigator.userAgent.match(/iPad/i)
-             || navigator.userAgent.match(/iPod/i)
-             || navigator.userAgent.match(/BlackBerry/i)
-         ) {
-             this.wnt.mobile = true;
-         }
-         if (navigator.userAgent.match(/MSIE/i)) {
-             this.wnt.ie = true;
-         }
-         if (this.wnt.mobile == true) {
-             navigator.geolocation.watchPosition(this.getPosition, this.positionError, {
-                 'enableHighAccuracy': true,
-                 'timeout': 10000,
-                 'maximumAge': 20000
-             });
-         } else{
-             alert("geo location is not available on this device!");
-         }
+'use strict';
 
 
-    this.getPosition = function(pos){
+class Model{
+
+    constructor() {
+        this.wnt = {};
+        this.wnt.mobile = false;
+        this.ie = false;
+        this.steps = localStorage.steps;
+    }
+
+    checkBrowser() {
+
+
+//check if mobile browser is supported
+        if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i)
+        ) {
+            this.wnt.mobile = true;
+        }
+        if (navigator.userAgent.match(/MSIE/i)) {
+            this.wnt.ie = true;
+        }
+        if (this.wnt.mobile === true) {
+            navigator.geolocation.watchPosition(this.getPosition, this.positionError,
+                {'enableHighAccuracy': true, 'timeout': 10000, 'maximumAge': 20000});
+        } else {
+            alert("Geo location is not available on this device");
+        }
+    }
+
+    /******** ONLOAD
+    $(function () {
+        $('#steps').ready(function () {
+            localStorage.clear();
+            wnt.steps = 0;
+            calcStepsTaken(wnt.steps);
+        });
+    });********/
+
+    getPosition(pos) {
+
         //let metersTraveled = pos.coords.speed * 10;   //NOTE: gets speed every 10 seconds, so (pos.coords.speed * 10)  ...meterspersec * sec... = meters traveled    ...   /0.762 = steps
         //!!!!! getting 8x the steps I should ... 2*10=20/0.762=26.2steps in 10 seconds???
         let steps = pos.coords.speed / 0.762;
-        this.stepsTaken(steps);
+        this.calcStepsTaken(steps);
+    }
 
-    };
-    this.positionError = function(err){
-        if(err.code==1){
+    positionError(err) {
+        if (err.code === 1) {
             alert("User denied geolocation.");
-        } else if(err.code==2){
+        } else if (err.code === 2) {
             alert("Position unavailable.");
-        } else if(err.code==3){
+        } else if (err.code === 3) {
             alert("Timeout expired.");
         } else {
-            alert("ERROR:"+ err.message);
+            alert("ERROR:" + err.message);
         }
-    };
+    }
 
-    this.stepsTaken = function(steps){
+    calcStepsTaken(steps) {
         this.wnt.steps = this.wnt.steps + steps;
         localStorage.steps = this.wnt.steps;
-        //$('#steps').html('<span style="color:#ff0000;">'+Math.round(this.wnt.steps)+'/6000 Steps</span>');
-        document.getElementById("steps").innerHTML = (Math.round(this.wnt.steps)).toString();
+        this.stepVal = (Math.round(this.wnt.steps)).toString();
+        document.getElementById("steps").innerHTML = this.stepVal;
+        //$('#steps').html('<span>' + Math.round(wnt.steps) + '/6000 Steps</span>');
+    }
 
-        console.log($('#steps').val());
-        localStorage.setItem('steps',parseInt(localStorage.steps) + parseInt($('#steps').val()));
-        console.log(localStorage.getItem('steps'));
-    };
+
 }
